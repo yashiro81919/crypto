64373a0d80e3522c95232098b06b1d05fdc8141d
yashiro81919 <yashiro81919@hotmail.com>
2024-11-18 20:37:41 +0800

    query addresses by pub key

--- pub_account.py
+++ pub_account.py
@@ -1,24 +1,30 @@
 from bitcoinlib.keys import HDKey
 from bitcoinlib.services.services import Service
 
-# add your master public key here
-xpub_key = "..."
+master_public_file_path = "public"
 
-def search_index(k: HDKey, srv: Service):
-    index = input("Index:")
-    ck = k.subkey_for_path("0/" + index)
-    print("address: " + ck.address())
+def list_addresses(k: HDKey, srv: Service):
+    for i in range(0, 20):
+        search_index(k, srv, str(i))
+
+def search_index(k: HDKey, srv: Service, i: str):
+    ck = k.subkey_for_path("0/" + i)
 
     balance = srv.getbalance(ck.address())
-    print("balance: " + str(balance/100000000))
+    print("|" + i + "|" + ck.address() + "|" + str(balance/100000000))
 
 
 if __name__ == "__main__":
+    with open(master_public_file_path, 'r') as file:
+        xpub_key = file.read()    
     k = HDKey(xpub_key, network="bitcoin")
     srv = Service()
     while True:
-        next = input("Please choose next step: [0-search] [1-exit]")
+        next = input("Please choose next step: [0-list 20 addresses] [1-search by index] [2-exit]")
         if next == "0":
-            search_index(k, srv)
+            list_addresses(k, srv)
+        elif next == "1":
+            index = input("Index:")
+            search_index(k, srv, index)
         else:
             exit()
